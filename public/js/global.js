833aafbf36cc962e93032fa3ecd43a2a2492e684
Navarr Barnier <me@navarr.me>
2013-09-05 20:55:50 -0400

    Show Date on Event Pages

--- public/js/global.js
+++ public/js/global.js
@@ -1,7 +1,7 @@
 $(function() {
     $('.datepicker').datepicker({startDate:'today', format:'d MM yyyy', autoclose: true});
 
-    $('time').each(function() {
+    $('.dateformat').each(function() {
         var $this = $(this);
         if ($this.data('unix') != undefined && $this.data('format') != undefined) {
             $this.text(date($this.data('format'), parseInt($this.data('unix'), 10)));
