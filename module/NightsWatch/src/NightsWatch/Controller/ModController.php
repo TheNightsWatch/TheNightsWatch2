cc8dd11bd4394cc48cb8ef726a5fec1a63a8896e
Navarr Barnier <me@navarr.me>
2014-07-07 22:46:39 -0400

    Implement a cape cache to hammer the server less

--- module/NightsWatch/src/NightsWatch/Controller/ModController.php
+++ module/NightsWatch/src/NightsWatch/Controller/ModController.php
@@ -19,7 +19,7 @@ class ModController extends ActionController
             ->getRepository('NightsWatch\Entity\User')
             ->findOneBy(['username' => $user]);
 
-        if (!$user || $user->rank == User::RANK_CIVILIAN) {
+        if (!$user) {
             $this->getResponse()->setStatusCode(404);
             return $this->getResponse();
         }
@@ -32,69 +32,77 @@ class ModController extends ActionController
         $response = new Response();
         $response->getHeaders()->addHeaders(['Content-Type' => 'image/png']);
 
-        $images = [];
-        $base = imagecreatefromstring(file_get_contents('data/capes/base.png'));
-        switch ($user->rank) {
-            default:
-                $iconType = null;
-                break;
-            case User::RANK_COMMANDER:
-                $iconType = 'commander';
-                break;
-            case User::RANK_GENERAL:
-                $iconType = 'general';
-                break;
-            case User::RANK_LIEUTENANT:
-                $iconType = 'lieutenant';
-                break;
-            case User::RANK_CORPORAL:
-                $iconType = 'corporal';
-                break;
-        }
-        switch ($user->order) {
-            case User::ORDER_RANGER:
-                $backingType = 'ranger';
-                 break;
-            case User::ORDER_STEWARD:
-                $backingType = 'steward';
-                break;
-        }
-        if ($user->rank == User::RANK_RECRUIT) {
-            $backingType = 'recruit';
-        }
-        if ($user->rank == User::RANK_COMMANDER) {
-            $backingType = 'commander';
-        }
-        $backing = imagecreatefromstring(file_get_contents("data/capes/backing-{$backingType}.png"));
-        $raven = imagecreatefromstring(file_get_contents("data/capes/raven-logo.png"));
-        $images = [$base, $backing, $raven];
-        if ($user->rank == User::RANK_RECRUIT) {
-            $recruit = imagecreatefromstring(file_get_contents("data/capes/recruit.png"));
-            $images[] = $recruit;
-        }
-        if (!is_null($iconType)) {
-            $icon = imagecreatefromstring(file_get_contents("data/capes/icon-{$iconType}.png"));
-            $images[] = $icon;
-        }
+        $cacheName = sha1(strtolower($user->username) . "-{$user->rank}-{$user->deserter}-{$user->order}");
+
+        $fileLoc = "data/cache/cape-{$cacheName}.png";
+        if (file_exists($fileLoc)) {
+            $imageContents = file_get_contents($fileLoc);
+        } else {
+            $images = [];
+            $base = imagecreatefromstring(file_get_contents('data/capes/base.png'));
+            switch ($user->rank) {
+                default:
+                    $iconType = null;
+                    break;
+                case User::RANK_COMMANDER:
+                    $iconType = 'commander';
+                    break;
+                case User::RANK_GENERAL:
+                    $iconType = 'general';
+                    break;
+                case User::RANK_LIEUTENANT:
+                    $iconType = 'lieutenant';
+                    break;
+                case User::RANK_CORPORAL:
+                    $iconType = 'corporal';
+                    break;
+            }
+            switch ($user->order) {
+                case User::ORDER_RANGER:
+                    $backingType = 'ranger';
+                    break;
+                case User::ORDER_STEWARD:
+                    $backingType = 'steward';
+                    break;
+            }
+            if ($user->rank == User::RANK_RECRUIT) {
+                $backingType = 'recruit';
+            }
+            if ($user->rank == User::RANK_COMMANDER) {
+                $backingType = 'commander';
+            }
+            $backing = imagecreatefromstring(file_get_contents("data/capes/backing-{$backingType}.png"));
+            $raven = imagecreatefromstring(file_get_contents("data/capes/raven-logo.png"));
+            $images = [$base, $backing, $raven];
+            if ($user->rank == User::RANK_RECRUIT) {
+                $recruit = imagecreatefromstring(file_get_contents("data/capes/recruit.png"));
+                $images[] = $recruit;
+            }
+            if (!is_null($iconType)) {
+                $icon = imagecreatefromstring(file_get_contents("data/capes/icon-{$iconType}.png"));
+                $images[] = $icon;
+            }
 
 
-        foreach($images as $image) {
-            imagesavealpha($image, true);
+            foreach ($images as $image) {
+                imagesavealpha($image, true);
+            }
+
+            $w = imagesx($base);
+            $h = imagesy($base);
+
+            array_shift($images); // remove $base from $images
+
+            foreach ($images as $image) {
+                imagecopyresampled($base, $image, 0, 0, 0, 0, $w, $h, $w, $h);
+            }
+
+            ob_start();
+            imagepng($base);
+            $imageContents = ob_get_contents();
+            ob_end_clean();
+            file_put_contents($fileLoc, $imageContents);
         }
-
-        $w = imagesx($base);
-        $h = imagesy($base);
-
-        array_shift($images); // remove $base from $images
-
-        foreach($images as $image) {
-            imagecopyresampled($base, $image, 0, 0, 0, 0, $w, $h, $w, $h);
-        }
-
-        ob_start();
-        imagepng($base);
-        $imageContents = ob_get_contents();
-        ob_end_clean();
 
         $response->setContent($imageContents);
         return $response;
