8ffc0084115f5728073eb3fb5a82642268774cbd
Navarr Barnier <me@navarr.me>
2015-02-12 22:34:59 -0500

    Add shotbowlogin route

--- module/NightsWatch/config/module.config.php
+++ module/NightsWatch/config/module.config.php
@@ -3,160 +3,170 @@
 namespace NightsWatch;
 
 return [
-    'controllers' => [
+    'controllers'     => [
         'invokables' => [
-            'Site' => 'NightsWatch\Controller\SiteController',
-            'Map' => 'NightsWatch\Controller\MapController',
-            'Chat' => 'NightsWatch\Controller\ChatController',
-            'Join' => 'NightsWatch\Controller\JoinController',
-            'Rules' => 'NightsWatch\Controller\RulesController',
+            'Site'         => 'NightsWatch\Controller\SiteController',
+            'Map'          => 'NightsWatch\Controller\MapController',
+            'Chat'         => 'NightsWatch\Controller\ChatController',
+            'Join'         => 'NightsWatch\Controller\JoinController',
+            'Rules'        => 'NightsWatch\Controller\RulesController',
             'Announcement' => 'NightsWatch\Controller\AnnouncementController',
-            'User' => 'NightsWatch\Controller\UserController',
-            'Event' => 'NightsWatch\Controller\EventController',
-            'Black' => 'NightsWatch\Controller\BlackController',
-            'Mod' => 'NightsWatch\Controller\ModController',
+            'User'         => 'NightsWatch\Controller\UserController',
+            'Event'        => 'NightsWatch\Controller\EventController',
+            'Black'        => 'NightsWatch\Controller\BlackController',
+            'Mod'          => 'NightsWatch\Controller\ModController',
         ],
     ],
-    'router' => [
+    'router'          => [
         'routes' => [
-            'home' => [
-                'type' => 'segment',
+            'home'         => [
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '[/][:controller][/:action]',
+                    'route'    => '[/][:controller][/:action]',
                     'defaults' => [
                         'controller' => 'site',
-                        'action' => 'index',
+                        'action'     => 'index',
                     ],
                 ],
             ],
-            'homePlural' => [
-                'type' => 'segment',
+            'homePlural'   => [
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '[/][:controller]s[/:action]',
+                    'route'    => '[/][:controller]s[/:action]',
                     'defaults' => [
                         'action' => 'index',
                     ],
                 ],
             ],
-            'rules' => [
-                'type' => 'segment',
+            'shotbowlogin' => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/rules[/:action]',
+                    'route'    => '/shotbowlogin',
+                    'defaults' => [
+                        'controller' => 'site',
+                        'action'     => 'shotbowlogin',
+                    ],
+                ],
+            ],
+            'rules'        => [
+                'type'    => 'segment',
+                'options' => [
+                    'route'    => '/rules[/:action]',
                     'defaults' => [
                         'controller' => 'rules',
-                        'action' => 'index',
+                        'action'     => 'index',
                     ],
                 ],
             ],
-            'mumble' => [
-                'type' => 'literal',
+            'mumble'       => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/mumble',
+                    'route'    => '/mumble',
                     'defaults' => [
                         'controller' => 'site',
-                        'action' => 'mumble',
+                        'action'     => 'mumble',
                     ],
                 ],
             ],
-            'mod' => [
-                'type' => 'literal',
+            'mod'          => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/mod',
+                    'route'    => '/mod',
                     'defaults' => [
                         'controller' => 'site',
-                        'action' => 'mod',
+                        'action'     => 'mod',
                     ],
                 ],
             ],
-            'login' => [
-                'type' => 'literal',
+            'login'        => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/login',
+                    'route'    => '/login',
                     'defaults' => [
                         'controller' => 'site',
-                        'action' => 'login',
+                        'action'     => 'login',
                     ],
                 ],
             ],
-            'logout' => [
-                'type' => 'literal',
+            'logout'       => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/logout',
+                    'route'    => '/logout',
                     'defaults' => [
                         'controller' => 'site',
-                        'action' => 'logout',
+                        'action'     => 'logout',
                     ],
                 ],
             ],
-            'calendar' => [
-                'type' => 'literal',
+            'calendar'     => [
+                'type'    => 'literal',
                 'options' => [
-                    'route' => '/calendar',
+                    'route'    => '/calendar',
                     'defaults' => [
                         'controller' => 'event',
-                        'action' => 'index',
+                        'action'     => 'index',
                     ],
                 ],
             ],
-            'id' => [
-                'type' => 'segment',
+            'id'           => [
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '[/]:controller/[:id][/:action]',
+                    'route'       => '[/]:controller/[:id][/:action]',
                     'constraints' => [
                         'id' => '\d+',
                     ],
-                    'defaults' => [
+                    'defaults'    => [
                         'action' => 'view',
                     ],
                 ],
             ],
-            'user' => [
-                'type' => 'segment',
+            'user'         => [
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '/user/[:username][/:action]',
+                    'route'    => '/user/[:username][/:action]',
                     'defaults' => [
                         'controller' => 'user',
-                        'action' => 'view',
+                        'action'     => 'view',
                     ],
                 ],
             ],
             'calendarDate' => [
-                'type' => 'segment',
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '/calendar/[:year]/[:month]/[:day]',
+                    'route'       => '/calendar/[:year]/[:month]/[:day]',
                     'constraints' => [
-                        'year' => '\d+',
+                        'year'  => '\d+',
                         'month' => '\d+',
-                        'day' => '\d+',
+                        'day'   => '\d+',
                     ],
-                    'defaults' => [
+                    'defaults'    => [
                         'controller' => 'event',
-                        'action' => 'date',
+                        'action'     => 'date',
                     ]
                 ]
             ],
             'takeTheBlack' => [
-                'type' => 'segment',
+                'type'    => 'segment',
                 'options' => [
-                    'route' => '/taketheblack/[:action]',
+                    'route'    => '/taketheblack/[:action]',
                     'defaults' => [
                         'controller' => 'black',
-                        'action' => 'index',
+                        'action'     => 'index',
                     ],
                 ],
             ],
         ],
     ],
-    'view_manager' => [
+    'view_manager'    => [
         'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
-        'template_path_stack' => [
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_path_stack'      => [
             __DIR__ . '/../view',
         ],
-        'strategies' => [
+        'strategies'               => [
             'ViewJsonStrategy',
         ],
     ],
@@ -165,65 +175,65 @@ return [
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
             'Zend\Log\LoggerAbstractServiceFactory',
         ],
-        'factories' => [
-            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+        'factories'          => [
+            'navigation'              => 'Zend\Navigation\Service\DefaultNavigationFactory',
             'right-noauth-navigation' => 'NightsWatch\Navigation\Service\RightNoAuthNavigationFactory',
-            'right-auth-navigation' => 'NightsWatch\Navigation\Service\RightAuthNavigationFactory',
+            'right-auth-navigation'   => 'NightsWatch\Navigation\Service\RightAuthNavigationFactory',
         ],
-        'aliases' => [
+        'aliases'            => [
             'translator' => 'MvcTranslator',
         ],
     ],
-    'translator' => [
-        'locale' => 'en_US',
+    'translator'      => [
+        'locale'                    => 'en_US',
         'translation_file_patterns' => [
             [
-                'type' => 'gettext',
+                'type'     => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern' => '%s.mo',
+                'pattern'  => '%s.mo',
             ],
         ],
     ],
-    'doctrine' => [
+    'doctrine'        => [
         'driver' => [
             __NAMESPACE__ . '_driver' => [
                 'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
                 'cache' => 'array',
                 'paths' => [__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'],
             ],
-            'orm_default' => [
+            'orm_default'             => [
                 'drivers' => [
                     __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
                 ],
             ],
         ],
     ],
-    'navigation' => [
-        'default' => [
+    'navigation'      => [
+        'default'      => [
             [
-                'label' => 'Home',
-                'route' => 'home',
+                'label'      => 'Home',
+                'route'      => 'home',
                 'controller' => 'site',
-                'action' => 'index',
+                'action'     => 'index',
             ],
             [
-                'label' => 'Chat',
-                'route' => 'home',
+                'label'      => 'Chat',
+                'route'      => 'home',
                 'controller' => 'chat',
             ],
             [
-                'label' => 'Announcements',
-                'route' => 'homePlural',
+                'label'      => 'Announcements',
+                'route'      => 'homePlural',
                 'controller' => 'announcement',
             ],
             [
-                'label' => 'Calendar',
-                'route' => 'calendar',
+                'label'      => 'Calendar',
+                'route'      => 'calendar',
                 'controller' => 'event',
             ],
             [
-                'label' => 'Map',
-                'route' => 'home',
+                'label'      => 'Map',
+                'route'      => 'home',
                 'controller' => 'map',
             ],
             [
@@ -235,8 +245,8 @@ return [
                         'route' => 'rules',
                     ],
                     [
-                        'label' => 'Members',
-                        'route' => 'homePlural',
+                        'label'      => 'Members',
+                        'route'      => 'homePlural',
                         'controller' => 'user',
                     ],
                     [
@@ -249,7 +259,7 @@ return [
                     ],
                     [
                         'label' => 'Subreddit',
-                        'uri' => 'https://reddit.com/r/TheNightsWatch',
+                        'uri'   => 'https://reddit.com/r/TheNightsWatch',
                     ]
                 ]
             ],
@@ -257,12 +267,10 @@ return [
         'right-noauth' => [
             [
                 'label' => 'Login or Register',
-                'route' => 'home',
-                'controller' => 'site',
-                'action' => 'shotbowlogin',
-            ]
+                'route' => 'shotbowlogin',
+            ],
         ],
-        'right-auth' => [
+        'right-auth'   => [
             [
                 'label' => 'Log Out',
                 'route' => 'logout',
